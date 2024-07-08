20bce9a2d3e748663d5fa8f2b01ac1f150e75bda
shirley_m <shirley_m@DESKTOP-9FPOJ1O>
2024-07-08 16:26:24 +0300

    Add project files.

---
+++ Triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int width = ReadInt("Enter width: ");
+         
+            int height = ReadInt("Enter height: ");
+
+            int result = Triangle(width, height);
+            Console.WriteLine($"The area is {result}");
+
+            Console.ReadLine();  
+
+        }
+        static int Triangle(int width, int height)
+        {
+            return (width * height) / 2;
+        }
+
+
+        static int ReadInt (string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
